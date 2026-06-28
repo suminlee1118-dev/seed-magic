13e264519de309e7f5fe6b96fbda16a49afb6d33
suminlee1118-dev <suminlee1118@gmail.com>
2026-06-28 18:07:58 +0900

    Update ResultScreen.jsx

--- src/pages/ResultScreen.jsx
+++ src/pages/ResultScreen.jsx
@@ -1,4 +1,4 @@
-\import { useState } from "react";
+import { useState } from "react";
 import { motion } from "framer-motion";
 import { useNavigate, useParams } from "react-router-dom";
 import { getSeedById, CORE_MESSAGE } from "../data/seeds";
